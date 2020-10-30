d1eb4eec79168b8e8281e11aa479fa5f892acfbb
acse-os920 <72440642+acse-os920@users.noreply.github.com>
2020-10-30 19:11:04 +0300

    Add files via upload

--- acse_la/det.py
+++ acse_la/det.py
@@ -2,7 +2,24 @@ import numpy as np
 
 
 def det(a):
+    """
+    Calculates the determinant of a square matrix 
+    of arbitrary size.
+    
+    Parameters
+    ----------
+    a : np.array or list of lists
+        'n x n' array
 
+    Examples
+    --------
+    >> a = [[2, 0, -1], [0, 5, 6], [0, -1, 1]]
+    >> det(a)
+    22.0
+    >> A = [[1, 0, -1], [-2, 3, 0], [1, -3, 2]]
+    >> det(A)
+    3.0
+    """
     det3 = 0
 
     #a = a.tolist()
